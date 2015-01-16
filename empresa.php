1cc64e8b7a41a3836ed2ba1c43259951b66411be
alvarocamu <alvaro.camunias92@gmail.com>
2015-01-16 12:23:44 +0100

    ejercicio resuelto de github

---
+++ empresa.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html  lang="es">
+<head>
+	<meta charset='utf-8'>
+	<title>Enviar datos por url</title>
+	<meta charset="utf-8" >
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.1/journal/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+
+
+<h2 style="text-transform: uppercase;"><?php echo $idioma=$_GET['idioma']; ?></h2>
+<?php
+
+$bio1 =<<<BIO1
+	<p>Esto es una pagina es español</p>
+BIO1;
+$bio2 =<<<BIO2
+	<p>This is a web in English</p>
+BIO2;
+$bio3 =<<<BIO3
+	<p>Ce est une page en français</p>
+BIO3;
+$bio4 =<<<BIO4
+	<p>Dies ist eine Seite in Deutsch</p>
+BIO4;
+	
+$idioma='';
+if(empty($_GET['idioma'])){
+	echo'';
+}else{
+	$idioma=$_GET['idioma'];		
+	if($idioma=='español')echo $bio1;
+	if($idioma=='ingles')echo $bio2;
+	if($idioma=='frances')echo $bio3;
+	if($idioma=='aleman')echo $bio4;
+}
+
+
+?>
+</div>
+</body>
+</html>
